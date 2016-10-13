f27a6a4a83cdc304a84e938b751a904cd1955da5
harrysame <harrysame@hotmail.com>
2016-10-13 23:48:30 +0800

    This is a digest on how to perform the data analysis on Horse Racing(Initial Version).

---
+++ digest.py
@@ -0,0 +1,21 @@
+'''
+This is a general guideline and instruction for 
+performing the analysis on Horse Racing in HK
+Crucial process are described below:
+------------------------------------------------------
+
+1)	Web scraping from http://www.hkjc.com/english/racing/SelectHorse.asp 
+	and related data sources
+
+2)	Data munging so as to feed the model
+
+3) 	Perform analysis using different models 
+	Promising tries including Regression,Decision Trees,
+	Monte Carlo,Ramdon Forest,etc..
+
+4)	Visualization and evaluation
+
+5)	Optimization and fine-tuning so as to get better outcome
+
+6)	To be continued...
+'''
